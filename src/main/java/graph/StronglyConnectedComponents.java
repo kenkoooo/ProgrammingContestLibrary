fadbafe4f1eb11c4e5cb444dfa5e0c3a6960c8f6
kenkoooo <kenkou.n@gmail.com>
2016-06-15 22:48:47 +0900

    Add SCC

---
+++ src/main/java/graph/StronglyConnectedComponents.java
@@ -0,0 +1,68 @@
+package graph;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class StronglyConnectedComponents {
+  public static int[] decomposite(ArrayList<Integer>[] G) {
+    ArrayList<Integer> vs = new ArrayList<Integer>();
+    int V = G.length;
+    int[] cmp = new int[V];
+
+    ArrayList<Integer>[] rG = new ArrayList[V];
+    for (int i = 0; i < V; i++) rG[i] = new ArrayList<Integer>();
+    for (int i = 0; i < V; i++) for (int v : G[i]) rG[v].add(i);
+    boolean[] used = new boolean[V];
+
+    ArrayDeque<Integer> stack = new ArrayDeque<>();
+    boolean[] added = new boolean[V];
+    for (int i = 0; i < V; i++)
+      if (!used[i]) {
+        stack.addFirst(i);
+        while (!stack.isEmpty()) {
+          int v = stack.peekFirst();
+          used[v] = true;
+          boolean pushed = false;
+          for (int j = G[v].size() - 1; j >= 0; j--) {
+            int u = G[v].get(j);
+            if (!used[u]) {
+              stack.addFirst(u);
+              pushed = true;
+            }
+          }
+          if (!pushed) {
+            stack.pollFirst();
+            if (!added[v]) {
+              vs.add(v);
+              added[v] = true;
+            }
+          }
+        }
+      }
+
+    used = new boolean[V];
+    int k = 0;
+    Collections.reverse(vs);
+    for (int i : vs)
+      if (!used[i]) {
+        stack.push(i);
+        used[i] = true;
+        cmp[i] = k;
+        while (!stack.isEmpty()) {
+          int v = stack.peek();
+          boolean pushed = false;
+          for (int u : rG[v])
+            if (!used[u]) {
+              used[u] = true;
+              cmp[u] = k;
+              stack.push(u);
+              pushed = true;
+            }
+          if (!pushed) stack.pop();
+        }
+        k++;
+      }
+    return cmp;
+  }
+}
---
+++ src/test/groovy/graph/StronglyConnectedComponentsTest.groovy
@@ -0,0 +1,43 @@
+package graph
+
+import spock.lang.Specification
+
+class StronglyConnectedComponentsTest extends Specification {
+    def "ランダムにグラフを作って到達可能かどうかで判断"() {
+        setup:
+        Random random = new Random()
+        10.times {
+            int N = 500
+            boolean[][] wf = new boolean[N][N]
+            ArrayList<Integer>[] g = new ArrayList<Integer>[N]
+            for (i in 0..<N) g[i] = new ArrayList<Integer>();
+            (N * 5).times {
+                int from = random.nextInt(N)
+                int to = random.nextInt(N)
+                g[from].add(to)
+                wf[from][to] = true
+            }
+
+            int[] cmp = StronglyConnectedComponents.decomposite(g)
+            for (int k = 0; k < N; k++) {
+                for (int i = 0; i < N; i++) {
+                    for (int j = 0; j < N; j++) {
+                        if (wf[i][k] && wf[k][j]) wf[i][j] = true
+                    }
+                }
+            }
+
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (wf[i][j] && wf[j][i]) {
+                        assert cmp[i] == cmp[j]
+                    } else if (wf[i][j]) {
+                        assert cmp[i] < cmp[j]
+                    } else if (wf[j][i]) {
+                        assert cmp[j] < cmp[i]
+                    }
+                }
+            }
+        }
+    }
+}
