5c86beb0509f64962b6eb92606d2f91054db822f
kenkoooo <kenkou.n@gmail.com>
2016-07-24 06:36:20 +0900

    :+1: RMQ のテストを追加

--- src/test/groovy/structure/RMQTest.groovy
+++ src/test/groovy/structure/RMQTest.groovy
@@ -23,4 +23,28 @@ class RMQTest extends Specification {
             }
         }
     }
+
+    def "update で構築する"() {
+        setup:
+        Random random = new Random()
+        100.times {
+            int N = 500
+            long[] array = new long[N]
+            RMQ rmq = new RMQ(N)
+            for (int i = 0; i < array.length; i++) {
+                array[i] = random.nextInt()
+                rmq.update(i, array[i])
+            }
+
+            for (int i = 0; i < N; i++) {
+                long min = Long.MAX_VALUE
+                for (int j = i; j < N; j++) {
+                    for (int k = i; k <= j; k++) {
+                        min = Math.min(min, array[j])
+                    }
+                    assert min == rmq.query(i, j + 1)
+                }
+            }
+        }
+    }
 }
