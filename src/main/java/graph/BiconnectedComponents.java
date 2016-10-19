9516a6ba332dd70646d38801c3922207057a2437
kenkoooo <kenkou.n@gmail.com>
2016-10-19 21:40:30 +0900

    :bug: BitSet にしてみた

--- src/main/java/graph/BiconnectedComponents.java
+++ src/main/java/graph/BiconnectedComponents.java
@@ -2,15 +2,15 @@ package graph;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.BitSet;
 
 public class BiconnectedComponents {
-
 
   private int[] order;
   private int[] stack;
   private int[] path;
   private int s = 0, t = 0;
-  private boolean[] inStack;
+  private BitSet inStack;
   private int idx;
   private int k;
   private final ArrayList<ArrayList<Integer>> G;
@@ -25,7 +25,7 @@ public class BiconnectedComponents {
     k = 0;
     order = new int[n];
     Arrays.fill(order, -1);
-    inStack = new boolean[n];
+    inStack = new BitSet(n);
     cmp = new int[n];
     stack = new int[n];
     path = new int[n];
@@ -43,14 +43,14 @@ public class BiconnectedComponents {
   private void dfs(int v, int p) {
     order[v] = idx++;
     stack[s++] = v;
-    inStack[v] = true;
+    inStack.set(v);
     path[t++] = v;
     for (Integer to : G.get(v)) {
       if (to == p) continue;
 
       if (order[to] == -1)
         dfs(to, v);
-      else if (inStack[to])
+      else if (inStack.get(to))
         while (order[path[t - 1]] > order[to]) t--;
     }
 
@@ -58,7 +58,7 @@ public class BiconnectedComponents {
       bridges.add(new int[]{p, v});
       while (true) {
         int w = stack[--s];
-        inStack[w] = false;
+        inStack.set(w, false);
         cmp[w] = k;
         if (v == w) break;
       }
