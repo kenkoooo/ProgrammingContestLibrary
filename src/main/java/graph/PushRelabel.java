53a351d1aba53e28fa76db9de2c9a6a72939cb97
kenkoooo <kenkou.n@gmail.com>
2016-12-20 21:17:21 +0900

    :+1: 微修正

--- src/main/java/graph/PushRelabel.java
+++ src/main/java/graph/PushRelabel.java
@@ -16,97 +16,95 @@ public class PushRelabel {
   }
 
   int N;
-  ArrayList<ArrayList<Edge>> G = new ArrayList<>();
-  long[] excess;
-  int[] dist, count;
-  boolean[] active;
-  ArrayDeque<Integer> Q = new ArrayDeque<>();
+  private ArrayList<ArrayList<Edge>> graph = new ArrayList<>();
+  private long[] excess;
+  private int[] height, count;
+  private boolean[] queued;
+  private ArrayDeque<Integer> queue = new ArrayDeque<>();
 
   PushRelabel(int N) {
     this.N = N;
-    for (int i = 0; i < N; i++) G.add(new ArrayList<>());
+    for (int i = 0; i < N; i++) graph.add(new ArrayList<>());
     excess = new long[N];
-    dist = new int[N];
-    active = new boolean[N];
+    height = new int[N];
+    queued = new boolean[N];
     count = new int[N * 2];
   }
 
   void push(Edge e) {
-    int amt = (int) Math.min(excess[e.from], e.cap - e.flow);
-    if (dist[e.from] <= dist[e.to] || amt == 0)
-      return;
-    e.flow += amt;
-    G.get(e.to).get(e.rev).flow -= amt;
-    excess[e.to] += amt;
-    excess[e.from] -= amt;
+    long pushableFlow = Math.min(excess[e.from], e.cap - e.flow);
+    if (height[e.from] <= height[e.to] || pushableFlow == 0) return;
+
+    e.flow += pushableFlow;
+    graph.get(e.to).get(e.rev).flow -= pushableFlow;
+    excess[e.to] += pushableFlow;
+    excess[e.from] -= pushableFlow;
     enqueue(e.to);
   }
 
   void addEdge(int from, int to, int cap) {
-    G.get(from).add(new Edge(from, to, cap, 0, G.get(to).size()));
-    G.get(to).add(new Edge(to, from, 0, 0, G.get(from).size() - 1));
+    graph.get(from).add(new Edge(from, to, cap, 0, graph.get(to).size()));
+    graph.get(to).add(new Edge(to, from, 0, 0, graph.get(from).size() - 1));
   }
 
-  void discharge(int v) {
-
-    for (Edge e : G.get(v)) {
-      if (excess[v] <= 0) break;
+  private void discharge(int u) {
+    for (Edge e : graph.get(u)) {
+      if (excess[u] == 0) break;
       push(e);
     }
-    if (excess[v] > 0) {
-      if (count[dist[v]] == 1)
-        gap(dist[v]);
+    if (excess[u] > 0) {
+      if (count[height[u]] == 1)
+        gap(height[u]);
       else
-        relabel(v);
+        relabel(u);
     }
   }
 
-  void enqueue(int v) {
-    if (!active[v] && excess[v] > 0) {
-      active[v] = true;
-      Q.push(v);
+  private void enqueue(int v) {
+    if (!queued[v] && excess[v] > 0) {
+      queued[v] = true;
+      queue.push(v);
     }
   }
 
-  void gap(int k) {
+  private void gap(int h) {
     for (int v = 0; v < N; v++) {
-      if (dist[v] < k)
-        continue;
-      count[dist[v]]--;
-      dist[v] = Math.max(dist[v], N + 1);
-      count[dist[v]]++;
+      if (height[v] < h) continue;
+      count[height[v]]--;
+      height[v] = Math.max(height[v], N + 1);
+      count[height[v]]++;
       enqueue(v);
     }
   }
 
   void relabel(int v) {
-    count[dist[v]]--;
-    dist[v] = 2 * N;
-    for (Edge e : G.get(v))
+    count[height[v]]--;
+    height[v] = 2 * N;
+    for (Edge e : graph.get(v))
       if (e.cap - e.flow > 0)
-        dist[v] = Math.min(dist[v], dist[e.to] + 1);
-    count[dist[v]]++;
+        height[v] = Math.min(height[v], height[e.to] + 1);
+    count[height[v]]++;
     enqueue(v);
   }
 
   long maxFlow(int s, int t) {
     count[0] = N - 1;
     count[N] = 1;
-    dist[s] = N;
-    active[s] = active[t] = true;
-    for (Edge e : G.get(s)) {
+    height[s] = N;
+    queued[s] = queued[t] = true;
+    for (Edge e : graph.get(s)) {
       excess[s] += e.cap;
       push(e);
     }
 
-    while (!Q.isEmpty()) {
-      int v = Q.poll();
-      active[v] = false;
+    while (!queue.isEmpty()) {
+      int v = queue.poll();
+      queued[v] = false;
       discharge(v);
     }
 
     long flow = 0;
-    for (Edge e : G.get(s)) flow += e.flow;
+    for (Edge e : graph.get(s)) flow += e.flow;
     return flow;
   }
 
