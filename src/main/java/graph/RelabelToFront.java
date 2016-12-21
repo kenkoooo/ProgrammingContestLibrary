22037d8cf1a610fed12680d65adf08db570162a7
kenkoooo <kenkou.n@gmail.com>
2016-12-21 21:01:55 +0900

    :+1: highest heuristics

--- src/main/java/graph/RelabelToFront.java
+++ src/main/java/graph/RelabelToFront.java
@@ -1,8 +1,6 @@
 package graph;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedList;
+import java.util.*;
 
 public class RelabelToFront {
   class Edge {
@@ -23,6 +21,13 @@ public class RelabelToFront {
   private int[] height;
   private int[] seen;
 
+  PriorityQueue<int[]> queue = new PriorityQueue<>(new Comparator<int[]>() {
+    @Override
+    public int compare(int[] o1, int[] o2) {
+      return -Integer.compare(o1[0], o2[0]);
+    }
+  });
+
   RelabelToFront(int N) {
     this.N = N;
     for (int i = 0; i < N; i++) graph.add(new ArrayList<>());
@@ -37,11 +42,17 @@ public class RelabelToFront {
   }
 
   private void push(Edge e) {
-    long send = Math.min(excess[e.from], e.cap - e.flow);
+    int u = e.from;
+    int v = e.to;
+
+    long send = Math.min(excess[u], e.cap - e.flow);
     e.flow += send;
-    graph.get(e.to).get(e.rev).flow -= send;
-    excess[e.from] -= send;
-    excess[e.to] += send;
+    graph.get(v).get(e.rev).flow -= send;
+    excess[u] -= send;
+    excess[v] += send;
+
+    if (excess[v] > 0) queue.add(new int[]{height[v], v});
+    if (excess[u] > 0) queue.add(new int[]{height[u], u});
   }
 
   private void relabel(int u) {
@@ -50,6 +61,8 @@ public class RelabelToFront {
       if (e.cap - e.flow > 0)
         minHeight = Math.min(minHeight, height[e.to]);
     height[u] = minHeight + 1;
+
+    queue.add(new int[]{height[u], u});
   }
 
   private void discharge(int u) {
@@ -68,26 +81,18 @@ public class RelabelToFront {
   }
 
   long maxFlow(int source, int sink) {
-    LinkedList<Integer> list = new LinkedList<>();
-    for (int i = 0; i < N; i++) {
-      if (i != source && i != sink) list.add(i);
-    }
     height[source] = N;
     for (Edge e : graph.get(source)) {
       excess[source] += e.cap;
       push(e);
     }
 
-    Iterator<Integer> it = list.iterator();
-    while (it.hasNext()) {
-      int u = it.next();
-      int oldHeight = height[u];
+    while (!queue.isEmpty()) {
+      int[] q = queue.poll();
+      int u = q[1];
+      if (excess[u] == 0) continue;
+      if (u == source || u == sink) continue;
       discharge(u);
-      if (height[u] > oldHeight) {
-        it.remove();
-        list.add(0, u);
-        it = list.iterator();
-      }
     }
 
     long maxFlow = 0;
